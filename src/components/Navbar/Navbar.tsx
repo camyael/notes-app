02a9e8a4bb80e07a926d56a91ae475b605e4d501
Camila Villaverde <camilavillaverde3@gmail.com>
2023-07-27 22:43:55 -0300

    responsive design

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,17 +1,39 @@
 import logo from "../../assets/logo/Logo_color.svg"
+import BurgerButton from "./BurgerButton";
+import { useState } from 'react';
 
 const Navbar = () => {
+    const [ menu, setMenu ] = useState<boolean>(false)
+    const [ scroll, setScroll ] = useState<boolean>(false)
+
+    const handleOnClick = () => {
+        setMenu(!menu)
+    }
+
+    const navbarScroll = () => {
+        if(window.scrollY >= 80) {
+            setScroll(true)
+        } else {
+            setScroll(false)
+        }
+    }
+
+    window.addEventListener('scroll', navbarScroll)
+
     return (
-        <header id="navbar">
+        <header id="navbar" className={scroll ? 'active' : ''}>
             <div className="navbar-logo">
-                <img src={logo} alt=""/>
+                <a href="/"><img src={logo} alt="logo"/></a>
             </div>
-            <nav className="navbar-menu">
+            <div>
+            <button className="navbar-button" onClick={handleOnClick}><BurgerButton/></button>
+            <nav className={`navbar-menu ${menu ? 'isActive' : ''}`}>
                 <a href="#">Servicio</a>
                 <a href="#">Nosotros</a>
                 <a href="#">Contacto</a>
                 <a href="#">Sumate</a>
             </nav>
+            </div>
         </header>
     )
 }
